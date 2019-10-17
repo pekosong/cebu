f269f3d1c97d8b9d79e69c414797b92c0e31cadd
pekosong <peko22@naver.com>
2019-10-17 23:14:14 +0900

    song

--- screens/Profile/ReservationScreen.js
+++ screens/Profile/ReservationScreen.js
@@ -15,7 +15,9 @@ import firebase from '../../constants/store';
 import {useSelector, useDispatch, shallowEqual} from 'react-redux';
 
 const MAP = {
-  wait: '예약중',
+  wait: '예약요청',
+  confirm: '예약완료',
+  end: '종료',
 };
 const WEEKMAP = {
   0: '월',
@@ -30,9 +32,12 @@ const WEEKMAP = {
 const ReservationScreen = props => {
   const {navigation} = props;
   const user = useSelector(state => state.user, shallowEqual);
-  const [notifications, setNotifications] = useState(true);
   const [dates, setDates] = useState([]);
   const [reservations, setReservations] = useState([]);
+  const [selectedReservations, setSelectedReservations] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [active, setActive] = useState('');
+  const [tabs, setTabs] = useState(['전체', '예약요청', '예약완료', '종료']);
 
   useEffect(() => {
     firebase
@@ -49,106 +54,137 @@ const ReservationScreen = props => {
         const days = new Set(reservations.map(e => e.date));
         setDates(Array.from(days));
         setReservations(reservations);
+        setSelectedReservations(reservations);
+        setIsLoaded(true);
+        setActive('전체');
       });
   }, []);
 
-  return (
+  renderReservationTab = tab => {
+    const isActive = active == tab;
+
+    return (
+      <TouchableOpacity
+        key={`tab-${tab}`}
+        onPress={() => handleTripTab(tab)}
+        style={[styles.tab, isActive ? styles.active : null]}>
+        <Text size={16} medium gray={!isActive} secondary={isActive}>
+          {tab}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  handleTripTab = tab => {
+    if (tab == '전체') {
+      setSelectedReservations(reservations);
+    } else {
+      setSelectedReservations(reservations.filter(e => MAP[e.status] == tab));
+    }
+    setActive(tab);
+  };
+
+  return isLoaded ? (
     <SafeAreaView>
+      <Block
+        style={{
+          flex: 0,
+          ...styles.header,
+        }}>
+        <Button onPress={() => navigation.goBack()} style={{width: 30}}>
+          <Ionicons
+            size={30}
+            color={theme.colors.black}
+            name="ios-arrow-back"
+          />
+        </Button>
+        <Text h1 bold>
+          예약 관리
+        </Text>
+      </Block>
+
+      <Block flex={false} row style={{...styles.tabs, marginBottom: 10}}>
+        {tabs.map(tab => renderReservationTab(tab))}
+      </Block>
+
       <ScrollView>
-        <Block style={styles.header}>
-          <Block row center space="between">
-            <Button onPress={() => navigation.goBack()}>
-              <Block center row>
-                <Ionicons
-                  size={30}
-                  color={theme.colors.black}
-                  name="ios-arrow-back"
-                />
-              </Block>
-            </Button>
-          </Block>
-          <Text h1 bold style={{marginTop: 10, marginBottom: 10}}>
-            예약 관리
-          </Text>
-        </Block>
-        <Block style={styles.inputs}>
-          <Divider></Divider>
-          {dates.length > 0 ? (
-            dates.map((day, idx) => (
-              <Fragment key={idx}>
-                <Block row>
-                  <Block center flex={false}>
-                    <Text h4 bold>
-                      {`${new Date(day).getMonth() + 1}월 ${new Date(
-                        day,
-                      ).getDate()}일`}
-                    </Text>
-                    <Text h4 bold style={{marginTop: 3}}>
-                      {`(${WEEKMAP[new Date(day).getDay()]})`}
-                    </Text>
-                  </Block>
-                  <Block>
-                    {reservations
-                      .filter(e => e.date == day)
-                      .map((e, eIdx) => {
-                        let maxLength =
-                          reservations.filter(e => e.date == day).length - 1;
-                        if (day == e.date) {
-                          return (
-                            <Fragment key={eIdx}>
-                              <TouchableOpacity>
-                                <Block row>
-                                  <Block style={{paddingHorizontal: 10}}>
-                                    <Block row>
-                                      <CachedImage
-                                        uri={e.image}
-                                        style={styles.avatarChat}></CachedImage>
-                                      <Block style={{marginLeft: 10}}>
-                                        <Text
-                                          bold
-                                          accent
-                                          style={{marginBottom: 3}}>
-                                          {e.name}
-                                        </Text>
-                                        <Text bold>{e.phone}</Text>
-                                      </Block>
-                                    </Block>
-                                    <Text bold style={{marginTop: 5}}>
-                                      예약시간: {e.time}, 예약인원: {e.people}
-                                    </Text>
-                                  </Block>
-                                  <Block flex={false}>
-                                    <Text bold h4 accent>
-                                      {MAP[e.status]}
-                                    </Text>
-                                  </Block>
-                                </Block>
-                              </TouchableOpacity>
-                              <Block
-                                style={{
-                                  marginBottom: maxLength == eIdx ? 0 : 10,
-                                  borderBottomWidth:
-                                    maxLength == eIdx ? 0 : 0.5,
-                                  borderBottomColor: theme.colors.gray2,
-                                  paddingBottom: maxLength == eIdx ? 0 : 10,
-                                }}></Block>
-                            </Fragment>
-                          );
-                        }
-                      })}
-                  </Block>
+        <Block style={{...styles.inputs}}>
+          {dates.map((day, idx) => (
+            <Fragment key={idx}>
+              <Block row>
+                <Block center flex={false}>
+                  <Text h4 bold>
+                    {`${new Date(day).getMonth() + 1}월 ${new Date(
+                      day,
+                    ).getDate()}일`}
+                  </Text>
+                  <Text h4 bold style={{marginTop: 3}}>
+                    {`(${WEEKMAP[new Date(day).getDay()]})`}
+                  </Text>
                 </Block>
-                <Divider></Divider>
-              </Fragment>
-            ))
-          ) : (
-            <ActivityIndicator
-              size="large"
-              color={theme.colors.primary}></ActivityIndicator>
-          )}
+                <Block>
+                  {selectedReservations
+                    .filter(e => e.date == day)
+                    .map((e, eIdx) => {
+                      let maxLength =
+                        selectedReservations.filter(e => e.date == day).length -
+                        1;
+                      if (day == e.date) {
+                        return (
+                          <Fragment key={eIdx}>
+                            <TouchableOpacity>
+                              <Block row>
+                                <Block style={{paddingHorizontal: 10}}>
+                                  <Block row>
+                                    <CachedImage
+                                      uri={e.image}
+                                      style={styles.avatarChat}></CachedImage>
+                                    <Block style={{marginLeft: 10}}>
+                                      <Text
+                                        bold
+                                        accent
+                                        style={{marginBottom: 3}}>
+                                        {e.name}
+                                      </Text>
+                                      <Text bold>{e.phone}</Text>
+                                    </Block>
+                                  </Block>
+                                  <Text bold style={{marginTop: 5}}>
+                                    예약시간: {e.time}, 예약인원: {e.people}
+                                  </Text>
+                                </Block>
+                                <Block flex={false}>
+                                  <Text bold h4 accent>
+                                    {MAP[e.status]}
+                                  </Text>
+                                </Block>
+                              </Block>
+                            </TouchableOpacity>
+                            <Block
+                              style={{
+                                marginBottom: maxLength == eIdx ? 0 : 10,
+                                borderBottomWidth: maxLength == eIdx ? 0 : 0.5,
+                                borderBottomColor: theme.colors.gray2,
+                                paddingBottom: maxLength == eIdx ? 0 : 10,
+                              }}></Block>
+                          </Fragment>
+                        );
+                      }
+                    })}
+                </Block>
+              </Block>
+              <Divider style={{marginLeft: 0, marginRight: 0}}></Divider>
+            </Fragment>
+          ))}
         </Block>
       </ScrollView>
     </SafeAreaView>
+  ) : (
+    <Block style={styles.full}>
+      <ActivityIndicator
+        size="large"
+        color={theme.colors.primary}></ActivityIndicator>
+    </Block>
   );
 };
 
@@ -174,12 +210,28 @@ const styles = StyleSheet.create({
   },
   inputs: {
     paddingHorizontal: theme.sizes.padding,
+    paddingTop: 20,
   },
 
   avatarChat: {
     width: theme.sizes.base * 3,
     height: theme.sizes.base * 3,
     borderRadius: theme.sizes.base * 1.5,
+  },
+  tabs: {
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginVertical:
+      Platform.OS === 'ios' ? theme.sizes.base * 0.8 : theme.sizes.base,
+    marginHorizontal: theme.sizes.padding,
+  },
+  tab: {
+    marginRight: theme.sizes.base,
+    paddingBottom: theme.sizes.base,
+  },
+  active: {
+    borderBottomColor: theme.colors.secondary,
+    borderBottomWidth: 3,
   },
 });
 
