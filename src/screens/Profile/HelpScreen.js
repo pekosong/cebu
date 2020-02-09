8850216e96db3668ca8ccab266ce38f39a3d08d2
pekosong <peko22@naver.com>
2020-02-09 23:14:28 +0900

    마이너 수정

--- src/screens/Profile/HelpScreen.js
+++ src/screens/Profile/HelpScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useEffect, useContext} from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 
@@ -7,72 +7,14 @@ import {colors, style} from 'app/src/styles';
 import {Ionicons} from '@expo/vector-icons';
 
 import {observer} from 'mobx-react-lite';
-import {updateUser} from 'app/src/api/user';
 import {UserStoreContext} from 'app/src/store/user';
 
 const HeloScreen = observer(props => {
   const {navigation} = props;
 
-  const [messageEmail, setMessageEmail] = useState(false);
-  const [messagePush, setMessagePush] = useState(false);
-  const [messageSms, setMessageSms] = useState(false);
-
-  const [noticeEmail, setNoticeEmail] = useState(false);
-  const [noticePush, setNoticePush] = useState(false);
-  const [noticeSms, setNoticeSms] = useState(false);
-
-  const [promotionEmail, setPromotionEmail] = useState(false);
-  const [promotionPush, setPromotionPush] = useState(false);
-  const [promotionSms, setPromotionSms] = useState(false);
-
   const {user} = useContext(UserStoreContext);
 
-  useEffect(() => {
-    const {notice} = user;
-
-    setMessageEmail(notice.message.email);
-    setMessagePush(notice.message.push);
-    setMessageSms(notice.message.sms);
-
-    setNoticeEmail(notice.notice.email);
-    setNoticePush(notice.notice.push);
-    setNoticeSms(notice.notice.sms);
-
-    setPromotionEmail(notice.promotion.email);
-    setPromotionPush(notice.promotion.push);
-    setPromotionSms(notice.promotion.sms);
-  }, []);
-
-  saveNotice = () => {
-    let newUser = {
-      ...user,
-      notice: {
-        message: {
-          email: messageEmail,
-          push: messagePush,
-          sms: messageSms,
-        },
-        notice: {
-          email: noticeEmail,
-          push: noticePush,
-          sms: noticeSms,
-        },
-        promotion: {
-          email: promotionEmail,
-          push: promotionPush,
-          sms: promotionSms,
-        },
-      },
-    };
-
-    updateUser(user.email, newUser)
-      .then(() => {
-        console.log('done');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  useEffect(() => {}, []);
 
   return (
     <SafeAreaView forceInset={{top: 'always'}} style={{flex: 1}}>
@@ -86,7 +28,7 @@ const HeloScreen = observer(props => {
           도움
         </Text>
         <Text darkgray h4 center style={{marginTop: 20}}>
-          저장 정보가 없습니다
+          준비 중입니다.
         </Text>
       </Block>
     </SafeAreaView>
--- src/screens/Profile/ProposalScreen.js
+++ src/screens/Profile/ProposalScreen.js
@@ -1,12 +1,42 @@
 import React, {useEffect} from 'react';
 import {StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
+import qs from 'qs';
+import {Linking} from 'react-native';
 
-import {Block, Text} from 'app/src/components';
+import {Block, Text, Button} from 'app/src/components';
 import {colors, sizes, style} from 'app/src/styles';
 import {Ionicons} from '@expo/vector-icons';
 
 import {observer} from 'mobx-react-lite';
+
+async function sendEmail(to, subject, body, options = {}) {
+  const {cc, bcc} = options;
+
+  let url = `mailto:${to}`;
+
+  // Create email link query
+  const query = qs.stringify({
+    subject: subject,
+    body: body,
+    cc: cc,
+    bcc: bcc,
+  });
+
+  if (query.length) {
+    url += `?${query}`;
+  }
+
+  // check if we can use this link
+  const canOpen = await Linking.canOpenURL(url);
+
+  if (!canOpen) {
+    throw new Error('Provided URL can not be handled');
+  }
+
+  return Linking.openURL(url);
+}
+
 const ProposalScreen = observer(props => {
   const {navigation} = props;
 
@@ -23,9 +53,28 @@ const ProposalScreen = observer(props => {
         <Text size={40} bold center>
           의견 남기기
         </Text>
-        <Text darkgray h4 center style={{marginTop: 20}}>
-          저장 정보가 없습니다
+        <Text darkgray h4 center style={{marginTop: 30}}>
+          불편하신 점 등
         </Text>
+        <Text darkgray h4 center style={{marginTop: 5, marginBottom: 30}}>
+          소중한 의견을 남겨주세요
+        </Text>
+        <Button
+          style={{
+            height: 60,
+            backgroundColor: colors.accent,
+            paddingHorizontal: 40,
+            borderRadius: 5,
+          }}
+          onPress={() =>
+            sendEmail('pekopeko11@gmail.com', '', '').then(() => {
+              console.log('Your message was successfully sent!');
+            })
+          }>
+          <Text h4 white bold center>
+            메일
+          </Text>
+        </Button>
       </Block>
     </SafeAreaView>
   );
